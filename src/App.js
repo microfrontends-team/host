49cac1e1c15ac1fbae631b2e29416674fba0b33e
IvanKharkivskyi1 <ikcomua@gmail.com>
2024-10-17 18:25:40 +0300

    docker

---
+++ src/App.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const App = () => <div>Microfrontend A (or B)</div>;
+
+export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,45 @@
-new ModuleFederationPlugin({
-    name: 'host',
-    remotes: {
-      microA: 'microA@http://localhost:3001/remoteEntry.js',
-      microB: 'microB@http://localhost:3002/remoteEntry.js',
+const { ModuleFederationPlugin } = require("webpack").container;
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  mode: "development",
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: 'auto',
+  },
+  devServer: {
+    port: 3000,
+    hot: false,
+    liveReload: false,
+    watchOptions: {
+      ignored: /node_modules/,
     },
-  });
-  
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        use: "babel-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: 'host',
+      remotes: {
+        microA: 'microA@http://localhost:3001/remoteEntry.js',
+      },
+      shared: { 
+        react: { singleton: true, eager: false, requiredVersion: '18' }, 
+        'react-dom': { singleton: true, eager: false, requiredVersion: '18' }
+      }
+    }),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+};
